afdaa2899b98f06d8e5553e0fd0b4cc55204e116
cmsbuild <>
2010-06-30 11:18:40 +0000

    This commit was manufactured by cvs2git to create tag 'DQM-L1TMonitorClient- CMSSW_3_8_0_pre8'.
    Sprout from master 2010-06-07 22:22:26 UTC Alex Tapper <sha1-11d34dd73d9f7a19fb1c333fabc2630e81f23142@cern.ch> 'Fixed failed assert in projections.'
    Cherrypick from master 2009-07-28 08:04:19 UTC Darren Michael Puigh <sha1-c46d72cb3e01124e714a162bae3295f66e002006@cern.ch> 'Update qtests for cosmics':
        data/L1TGMT_QualityTests.xml
    Cherrypick from master 2010-06-30 11:18:37 UTC Alex Tapper <sha1-11d34dd73d9f7a19fb1c333fabc2630e81f23142@cern.ch> 'Fix for harvesting.':
        src/L1TGCTClient.cc
    Delete:
        bin/BuildFile
        bin/HDQMInspectorConfigL1Trigger.h
        bin/Inspector.cpp
        bin/L1THistoryDQMService.cc
        bin/L1THistoryDQMService.h
        bin/L1TriggerHDQMInspector.cc
        bin/SealModules.cc
        bin/test/data/RunSummary_dt_rpc_csc_ecal_hcal_CRAFT09_1M.txt
        bin/test/runDBMaker.pl
        bin/test/runInspectorOnly.pl
        bin/test/submitMacro.ch

--- src/L1TGCTClient.cc
+++ src/L1TGCTClient.cc
@@ -40,49 +40,91 @@ void L1TGCTClient::beginJob(void)
   // Set to directory with ME in
   dbe_->setCurrentFolder(monitorDir_);
   
-  l1GctIsoEmOccEta_ = dbe_->book1D("IsoEmOccEta","ISO EM #eta OCCUPANCY", ETABINS, ETAMIN, ETAMAX);
-  l1GctIsoEmOccPhi_ = dbe_->book1D("IsoEmOccPhi","ISO EM #phi OCCUPANCY", PHIBINS, PHIMIN, PHIMAX);
-  l1GctNonIsoEmOccEta_ = dbe_->book1D("NonIsoEmOccEta","NON-ISO EM #eta OCCUPANCY", ETABINS, ETAMIN, ETAMAX);
-  l1GctNonIsoEmOccPhi_ = dbe_->book1D("NonIsoEmOccPhi","NON-ISO EM #phi OCCUPANCY", PHIBINS, PHIMIN, PHIMAX);
-  l1GctAllJetsOccEta_ = dbe_->book1D("AllJetsOccEta","CENTRAL AND FORWARD JET #eta OCCUPANCY", ETABINS, ETAMIN, ETAMAX);
-  l1GctAllJetsOccPhi_ = dbe_->book1D("AllJetsOccPhi","CENTRAL AND FORWARD JET #phi OCCUPANCY", PHIBINS, PHIMIN, PHIMAX);
-  l1GctCenJetsOccEta_ = dbe_->book1D("CenJetsOccEta","CENTRAL JET #eta OCCUPANCY", ETABINS, ETAMIN, ETAMAX);
-  l1GctCenJetsOccPhi_ = dbe_->book1D("CenJetsOccPhi","CENTRAL JET #phi OCCUPANCY", PHIBINS, PHIMIN, PHIMAX);
-  l1GctForJetsOccEta_ = dbe_->book1D("ForJetsOccEta","FORWARD JET #eta OCCUPANCY", ETABINS, ETAMIN, ETAMAX);
-  l1GctForJetsOccPhi_ = dbe_->book1D("ForJetsOccPhi","FORWARD JET #phi OCCUPANCY", PHIBINS, PHIMIN, PHIMAX);
-  l1GctTauJetsOccEta_ = dbe_->book1D("TauJetsOccEta","TAU JET #eta OCCUPANCY", ETABINS, ETAMIN, ETAMAX);
-  l1GctTauJetsOccPhi_ = dbe_->book1D("TauJetsOccPhi","TAU JET #phi OCCUPANCY", PHIBINS, PHIMIN, PHIMAX);
+  l1GctIsoEmOccEta_ = dbe_->book1D("IsoEmOccEta","ISO EM  #eta OCCUPANCY", ETABINS, ETAMIN, ETAMAX);
+  l1GctIsoEmOccPhi_ = dbe_->book1D("IsoEmOccPhi","ISO EM  #phi OCCUPANCY", PHIBINS, PHIMIN, PHIMAX);
+  l1GctNonIsoEmOccEta_ = dbe_->book1D("NonIsoEmOccEta","NON-ISO EM  #eta OCCUPANCY", ETABINS, ETAMIN, ETAMAX);
+  l1GctNonIsoEmOccPhi_ = dbe_->book1D("NonIsoEmOccPhi","NON-ISO EM  #phi OCCUPANCY", PHIBINS, PHIMIN, PHIMAX);
+  l1GctAllJetsOccEta_ = dbe_->book1D("AllJetsOccEta","CENTRAL AND FORWARD JET  #eta OCCUPANCY", ETABINS, ETAMIN, ETAMAX);
+  l1GctAllJetsOccPhi_ = dbe_->book1D("AllJetsOccPhi","CENTRAL AND FORWARD JET  #phi OCCUPANCY", PHIBINS, PHIMIN, PHIMAX);
+  l1GctCenJetsOccEta_ = dbe_->book1D("CenJetsOccEta","CENTRAL JET  #eta OCCUPANCY", ETABINS, ETAMIN, ETAMAX);
+  l1GctCenJetsOccPhi_ = dbe_->book1D("CenJetsOccPhi","CENTRAL JET  #phi OCCUPANCY", PHIBINS, PHIMIN, PHIMAX);
+  l1GctForJetsOccEta_ = dbe_->book1D("ForJetsOccEta","FORWARD JET  #eta OCCUPANCY", ETABINS, ETAMIN, ETAMAX);
+  l1GctForJetsOccPhi_ = dbe_->book1D("ForJetsOccPhi","FORWARD JET  #phi OCCUPANCY", PHIBINS, PHIMIN, PHIMAX);
+  l1GctTauJetsOccEta_ = dbe_->book1D("TauJetsOccEta","TAU JET  #eta OCCUPANCY", ETABINS, ETAMIN, ETAMAX);
+  l1GctTauJetsOccPhi_ = dbe_->book1D("TauJetsOccPhi","TAU JET  #phi OCCUPANCY", PHIBINS, PHIMIN, PHIMAX);
 }
 
 void L1TGCTClient::beginRun(const Run& r, const EventSetup& context) {}
 
 void L1TGCTClient::beginLuminosityBlock(const LuminosityBlock& lumiSeg, const EventSetup& context) {}
 
-void L1TGCTClient::endLuminosityBlock(const edm::LuminosityBlock& lumiSeg, const edm::EventSetup& c) {}
-
-void L1TGCTClient::analyze(const Event& e, const EventSetup& context)
+void L1TGCTClient::endLuminosityBlock(const edm::LuminosityBlock& lumiSeg, const edm::EventSetup& c) 
 {
-  makeXProjection(dbe_->get("L1T/L1TGCT/IsoEmOccEtaPhi")->getTH2F(),l1GctIsoEmOccEta_);
-  makeYProjection(dbe_->get("L1T/L1TGCT/IsoEmOccEtaPhi")->getTH2F(),l1GctIsoEmOccPhi_);
+  if (dbe_->get("L1T/L1TGCT/IsoEmOccEtaPhi")!=NULL){
+    makeXProjection(dbe_->get("L1T/L1TGCT/IsoEmOccEtaPhi")->getTH2F(),l1GctIsoEmOccEta_);
+    makeYProjection(dbe_->get("L1T/L1TGCT/IsoEmOccEtaPhi")->getTH2F(),l1GctIsoEmOccPhi_);
+  }
 
-  makeXProjection(dbe_->get("L1T/L1TGCT/NonIsoEmOccEtaPhi")->getTH2F(),l1GctNonIsoEmOccEta_);
-  makeYProjection(dbe_->get("L1T/L1TGCT/NonIsoEmOccEtaPhi")->getTH2F(),l1GctNonIsoEmOccPhi_);
+  if (dbe_->get("L1T/L1TGCT/NonIsoEmOccEtaPhi")!=NULL){
+    makeXProjection(dbe_->get("L1T/L1TGCT/NonIsoEmOccEtaPhi")->getTH2F(),l1GctNonIsoEmOccEta_);
+    makeYProjection(dbe_->get("L1T/L1TGCT/NonIsoEmOccEtaPhi")->getTH2F(),l1GctNonIsoEmOccPhi_);
+  }
 
-  makeXProjection(dbe_->get("L1T/L1TGCT/AllJetsOccEtaPhi")->getTH2F(),l1GctAllJetsOccEta_);
-  makeYProjection(dbe_->get("L1T/L1TGCT/AllJetsOccEtaPhi")->getTH2F(),l1GctAllJetsOccPhi_);
+  if (dbe_->get("L1T/L1TGCT/AllJetsOccEtaPhi")!=NULL){
+    makeXProjection(dbe_->get("L1T/L1TGCT/AllJetsOccEtaPhi")->getTH2F(),l1GctAllJetsOccEta_);
+    makeYProjection(dbe_->get("L1T/L1TGCT/AllJetsOccEtaPhi")->getTH2F(),l1GctAllJetsOccPhi_);
+  }
 
-  makeXProjection(dbe_->get("L1T/L1TGCT/CenJetsOccEtaPhi")->getTH2F(),l1GctCenJetsOccEta_);
-  makeYProjection(dbe_->get("L1T/L1TGCT/CenJetsOccEtaPhi")->getTH2F(),l1GctCenJetsOccPhi_);
+  if (dbe_->get("L1T/L1TGCT/CenJetsOccEtaPhi")!=NULL){
+    makeXProjection(dbe_->get("L1T/L1TGCT/CenJetsOccEtaPhi")->getTH2F(),l1GctCenJetsOccEta_);
+    makeYProjection(dbe_->get("L1T/L1TGCT/CenJetsOccEtaPhi")->getTH2F(),l1GctCenJetsOccPhi_);
+  }
 
-  makeXProjection(dbe_->get("L1T/L1TGCT/ForJetsOccEtaPhi")->getTH2F(),l1GctForJetsOccEta_);
-  makeYProjection(dbe_->get("L1T/L1TGCT/ForJetsOccEtaPhi")->getTH2F(),l1GctForJetsOccPhi_);
-
-  makeXProjection(dbe_->get("L1T/L1TGCT/TauJetsOccEtaPhi")->getTH2F(),l1GctTauJetsOccEta_);
-  makeYProjection(dbe_->get("L1T/L1TGCT/TauJetsOccEtaPhi")->getTH2F(),l1GctTauJetsOccPhi_);
+  if (dbe_->get("L1T/L1TGCT/ForJetsOccEtaPhi")!=NULL){
+    makeXProjection(dbe_->get("L1T/L1TGCT/ForJetsOccEtaPhi")->getTH2F(),l1GctForJetsOccEta_);
+    makeYProjection(dbe_->get("L1T/L1TGCT/ForJetsOccEtaPhi")->getTH2F(),l1GctForJetsOccPhi_);
+  }
+  
+  if (dbe_->get("L1T/L1TGCT/TauJetsOccEtaPhi")!=NULL){
+    makeXProjection(dbe_->get("L1T/L1TGCT/TauJetsOccEtaPhi")->getTH2F(),l1GctTauJetsOccEta_);
+    makeYProjection(dbe_->get("L1T/L1TGCT/TauJetsOccEtaPhi")->getTH2F(),l1GctTauJetsOccPhi_);
+  }
 }
 
+void L1TGCTClient::analyze(const Event& e, const EventSetup& context){}
 
-void L1TGCTClient::endRun(const Run& r, const EventSetup& context){}
+void L1TGCTClient::endRun(const Run& r, const EventSetup& context)
+{
+  if (dbe_->get("L1T/L1TGCT/IsoEmOccEtaPhi")!=NULL){
+    makeXProjection(dbe_->get("L1T/L1TGCT/IsoEmOccEtaPhi")->getTH2F(),l1GctIsoEmOccEta_);
+    makeYProjection(dbe_->get("L1T/L1TGCT/IsoEmOccEtaPhi")->getTH2F(),l1GctIsoEmOccPhi_);
+  }
+
+  if (dbe_->get("L1T/L1TGCT/NonIsoEmOccEtaPhi")!=NULL){
+    makeXProjection(dbe_->get("L1T/L1TGCT/NonIsoEmOccEtaPhi")->getTH2F(),l1GctNonIsoEmOccEta_);
+    makeYProjection(dbe_->get("L1T/L1TGCT/NonIsoEmOccEtaPhi")->getTH2F(),l1GctNonIsoEmOccPhi_);
+  }
+
+  if (dbe_->get("L1T/L1TGCT/AllJetsOccEtaPhi")!=NULL){
+    makeXProjection(dbe_->get("L1T/L1TGCT/AllJetsOccEtaPhi")->getTH2F(),l1GctAllJetsOccEta_);
+    makeYProjection(dbe_->get("L1T/L1TGCT/AllJetsOccEtaPhi")->getTH2F(),l1GctAllJetsOccPhi_);
+  }
+
+  if (dbe_->get("L1T/L1TGCT/CenJetsOccEtaPhi")!=NULL){
+    makeXProjection(dbe_->get("L1T/L1TGCT/CenJetsOccEtaPhi")->getTH2F(),l1GctCenJetsOccEta_);
+    makeYProjection(dbe_->get("L1T/L1TGCT/CenJetsOccEtaPhi")->getTH2F(),l1GctCenJetsOccPhi_);
+  }
+
+  if (dbe_->get("L1T/L1TGCT/ForJetsOccEtaPhi")!=NULL){
+    makeXProjection(dbe_->get("L1T/L1TGCT/ForJetsOccEtaPhi")->getTH2F(),l1GctForJetsOccEta_);
+    makeYProjection(dbe_->get("L1T/L1TGCT/ForJetsOccEtaPhi")->getTH2F(),l1GctForJetsOccPhi_);
+  }
+  
+  if (dbe_->get("L1T/L1TGCT/TauJetsOccEtaPhi")!=NULL){
+    makeXProjection(dbe_->get("L1T/L1TGCT/TauJetsOccEtaPhi")->getTH2F(),l1GctTauJetsOccEta_);
+    makeYProjection(dbe_->get("L1T/L1TGCT/TauJetsOccEtaPhi")->getTH2F(),l1GctTauJetsOccPhi_);
+  }
+}
 
 void L1TGCTClient::endJob(){}
 
@@ -90,26 +132,28 @@ void L1TGCTClient::makeXProjection(TH2F* input, MonitorElement* output)
 {
   // Are the provided input and output consistent
   if (input->GetNbinsX() != output->getNbinsX()) return;
-
+  
   // Make the projection
   TH1D* projX = input->ProjectionX();
-
+  
   for (Int_t i=0; i<projX->GetNbinsX(); i++) {
-    output->setBinContent(i,projX->GetBinContent(i));
+    output->setBinContent(i+1,projX->GetBinContent(i+1));
   }
+  delete projX;
 }
 
 void L1TGCTClient::makeYProjection(TH2F* input, MonitorElement* output)
 {
   // Are the provided input and output consistent
-  if (input->GetNbinsY() != output->getNbinsY()) return;
-
+  if (input->GetNbinsY() != output->getNbinsX()) return;
+  
   // Make the projection
   TH1D* projY = input->ProjectionY();
-
-  for (Int_t i=0; i<projY->GetNbinsY(); i++) {
-    output->setBinContent(i,projY->GetBinContent(i));
+  
+  for (Int_t i=0; i<projY->GetNbinsX(); i++) {
+    output->setBinContent(i+1,projY->GetBinContent(i+1));
   }
+  delete projY;
 }
 
 
