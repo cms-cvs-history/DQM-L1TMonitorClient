cd8772dac687ec5d2e6eec55f01c49b3fabc7c0b
Vasile Mihai Ghete <sha1-bda472ebd3845b96039bd3194f2d7659260e27b4@cern.ch>
2010-11-16 14:05:17 +0000

    L1 Trigger DQM redesign files, based on quality tests.

--- bin/SealModules.cc
+++ bin/SealModules.cc
@@ -1,13 +1,11 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/Framework/interface/SourceFactory.h"
-DEFINE_SEAL_MODULE();
-
 #include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
 
 #include "DQM/L1TMonitorClient/bin/L1THistoryDQMService.h"
-DEFINE_ANOTHER_FWK_SERVICE(L1THistoryDQMService);
+DEFINE_FWK_SERVICE(L1THistoryDQMService);
 
 #include "CondCore/PopCon/interface/PopConAnalyzer.h"
 #include "DQMServices/Diagnostic/interface/DQMHistoryPopConHandler.h"
 typedef popcon::PopConAnalyzer< popcon::DQMHistoryPopConHandler<L1THistoryDQMService > > L1TDQMHistoryPopCon;
-DEFINE_ANOTHER_FWK_MODULE(L1TDQMHistoryPopCon);
+DEFINE_FWK_MODULE(L1TDQMHistoryPopCon);
